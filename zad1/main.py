b0cb64d2673230379691967d8e89ca62880cc68a
Justyna <justiluc@interia.pl>
2022-01-21 13:40:09 +0100

    lab 15 zad 1

---
+++ zad1/main.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class MyTestCase(unittest.TestCase):
+    def test_title(self):
+        driver = webdriver.Chrome(executable_path='D:/uczelnia/II rok/semestr 3/testowanie_automatyczne/lab15/chromedriver')
+        driver.implicitly_wait(10)
+        driver.get("https://duckduckgo.com/")
+        # driver.find_element()
+        # time.sleep(5)
+        driver.find_element_by_id("search_form_input_homepage").send_keys("Selenium")
+        time.sleep(5)
+        driver.find_element_by_id("search_button_homepage").submit()
+        time.sleep(5)
+        self.assertEqual(driver.title, "Selenium at DuckDuckGo")
+        driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
